3ebb40ded8681c3a016e5618d5d09c976e65ab84
Dheeraj <Dheeraj77253@gmail.com>
2020-06-05 21:55:44 +0530

    create express app

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//Project Started
+const express = require('express');
+const port = 8000;
+const app = express();
+
+//running server
+app.listen(port,function(err)
+{
+    if(err)
+    {
+        console.log('Error in running server');
+        return;
+    }
+    console.log('Server is running and up at port ',port);
+    return;
+});

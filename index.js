231e93e9d9be784d68ee95de302ceb73bb88617f
Dheeraj <Dheeraj77253@gmail.com>
2020-06-06 13:40:11 +0530

    add router, controller and views setting

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const express = require('express');
 const port = 8000;
 const app = express();
 
+//parsing form data
+app.use(express.urlencoded());
+
+//using static files
+app.use(express.static('./assets'));
+
 //setting template engine
-app.set('viwe engine','ejs');
+app.set('view engine','ejs');
 app.set('views','./views');
+
+//using router 
+app.use('/',require('./routes/index.js'));
 
 //running server
 app.listen(port,function(err)
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+console.log("Router loaded");
+
+//accessing home controller
+var homeController = require('../controllers/home_controller');
+
+//routes
+router.get('/',homeController.home);
+
+
+module.exports = router;
